bae5809b64f78ba1879f6b4a5f5740ea7b007c31
Bodhi14 <bodhi140802@gmail.com>
2022-06-15 00:46:03 +0530

    Bot

---
+++ bot.py
@@ -0,0 +1,54 @@
+import discord
+import random
+import pyjokes
+
+TOKEN = "OTcyNTA3Njk0MzE1NDc1MDA2.GCshMl.cMk_Fg5d11IIhiH3SCiiinwODpe_aRrnlQ6_xs"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("The bot has logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user :
+        return
+
+    if message.channel.name == 'welcome-and-rules':
+        if user_message.lower() == 'hello':
+            greetings = ["ki khobor ?", "Long Time !!!", "What's Up buddy ?"]
+            hellos = ["Adios", "Hi"]
+            val = random.randint(0, len(greetings))
+            j = random. randint(0, len(hellos))
+
+            await message.channel.send(f'{hellos[j]} {username}!! + {greetings[val]} ')
+            return
+        elif user_message.lower() == 'bye':
+            byes = ["Adieu", "Sayonara", "Bye"]
+            r = random.randint(0, len(byes))
+            await message.channel.send(f'{byes[r]} {username}, see you later!!!')
+            return
+        elif user_message.lower() == '!joke':
+            await message.channel.send('Here is the joke:')
+            joke = pyjokes.get_joke(language='en', category='neutral')
+            await message.channel.send(joke)
+            return
+
+    if user_message.lower() == '!hibot':
+        await message.channel.send(f'Hello {username} !, ei bot tar puro functionality explore korar icche thakle, "welcome-and-rules" channel tae explore korte parish')
+        return
+
+
+client.run(TOKEN)
+
+
+
+
+
